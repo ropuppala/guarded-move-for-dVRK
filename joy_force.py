5193c1de9466442808b32d190e8372609f559ac1
Jennifer Hu <yhu32@jhu.edu>
2016-03-08 13:10:29 -0500

    fix some errors

--- joy_force.py
+++ joy_force.py
@@ -1,18 +1,22 @@
-from joy_control import *
+import sys
+import rospy
+from geometry_msgs.msg import Wrench
 
 class joy_force():
     def callback(self, data):
-        self._force_x = data.force.x
-        self._force_y = data.force.z
+        self._f_x = data.force.x
+        self._f_y = data.force.z
 
     def __init__(self, ros_namespace = '/joystick1/wrench'):
-        self._force_x = 0.0
-        self._force_y = 0.0
-        rospy.Subscriber('/joystick1/wrench', Joy, self.callback)
+        self._f_x = 0.0
+        self._f_y = 0.0
+        self.previous = 0
+        rospy.Subscriber('/joystick1/wrench', Wrench, self.callback)
 
     def zero_force (self):
-        if (self._force_x == 0 and self._force.y == 0):
+        if (self._f_x == 0.0 and self._f_y == 0.0 and self.previous == 0):
             return True
         else:
+            self.previous = False
             print 'out'
             return False
