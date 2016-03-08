0889c076b35658e3da65748a3af815d9046a5bf9
Jennifer Hu <yhu32@jhu.edu>
2016-03-08 13:10:04 -0500

    simplify code and get current position of the robot

--- 3D_mouse.py
+++ 3D_mouse.py
@@ -1,5 +1,5 @@
 import sys
-from dvrk.arm import * 
+from dvrk.psm import * 
 from sensor_msgs.msg import Joy
 import rospy
 import time
@@ -11,7 +11,7 @@ class calibration_testing:
 
     def __init__(self, robotName):
         self._robot_name = robotName
-        self._robot = arm(self._robot_name)
+        self._robot = psm(self._robot_name)
         self._last_axes = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         self._last_buttons = [0, 0]
         self.previous_mouse_buttons = [0, 0]
@@ -29,15 +29,15 @@ class calibration_testing:
         return self._last_buttons
     
     def run(self):
-        d2r = math.pi / 180
-        recorded_joint_positions = []
-        recorded_cartesian_positions = []
+        #d2r = math.pi / 180
+        #recorded_joint_positions = []
+        #recorded_cartesian_positions = []
         sample_nb = 0
         acceleration_counter = 1.0
-        range_of_motion = [ [-40 * d2r, 40 * d2r], [-40 * d2r, 40 * d2r], [-40 * d2r, 40 * d2r]]
+        #range_of_motion = [ [-40 * d2r, 40 * d2r], [-40 * d2r, 40 * d2r], [-40 * d2r, 40 * d2r]]
         density = 3
-        joint_motions = [ 0, 0, 0]
-        joint_indexs = [ 0, 0, 0]
+        #joint_motions = [ 0, 0, 0]
+        #joint_indexs = [ 0, 0, 0]
         
         while sample_nb < ((density ** 3) + 1):
             if self._last_axes[0] != 0 or self._last_axes[1] != 0 or self._last_axes[2] != 0:
@@ -51,39 +51,13 @@ class calibration_testing:
             #move based on mouse position
             self._robot.delta_move_cartesian_translation([y, -x, z], False)
             if self.mouse_buttons()[0] == 1 and self.previous_mouse_buttons[0] == 0:
-                shaft_remainder = sample_nb % (density ** 2)
-                shaft_index = (sample_nb - shaft_remainder) / (density ** 2)
-                finger_index = shaft_remainder % density
-                wrist_index = (shaft_remainder - finger_index) / density
-                joint_indexs = [shaft_index, wrist_index, finger_index]
-                for joint in range(3):
-                    joint_motions[joint] = range_of_motion[joint][0] + (joint_indexs[joint]*((range_of_motion[joint][1] - range_of_motion[joint][0])/(density -1)))
-                #move to next joint position
-                self._robot.delta_move_cartesian_translation([0.0,0.0,0.05])
-                time.sleep(.2)
-                self._robot.move_joint_list([joint_motions[0], joint_motions[1], joint_motions[2], 0.0],[3,4,5,6])
-                time.sleep(.2)
-                #move close to next cartesian position
-                cartesian_totals = []
-                average_cartesian_positions = []
-                for axis in range(3):
-                    for cartesian_sample in range(len(recorded_cartesian_positions)):
-                        cartesian_totals.append(recorded_cartesian_positions[cartesian_sample][axis])
-                    average_cartesian_positions.append(sum(cartesian_totals)/len(cartesian_totals))
-                    cartesian_totals = []
-                self._robot.move_cartesian_translation([average_cartesian_positions[0], average_cartesian_positions[1], average_cartesian_positions[2] + 0.005])              
-                time.sleep(.2)
-                sample_nb += 1
-                if sample_nb < 28:
-                    print "you are on sample: ", sample_nb, " / ", density**3
-                if sample_nb >= 28:
-                    print "finished"
-            
-        
+                print 'Current: ', self._robot.get_current_cartesian_position()
             if self.mouse_buttons()[1] == 1 and self.previous_mouse_buttons[1] == 0:
-                self.open_jaw()
-            if self.mouse_buttons()[1] == 0 and self.previous_mouse_buttons[1] == 1:
-                self.close_jaw()
+                #if (self._robot.get_current_joint_position()[6] >= (35 * math.pi / 180)):
+                self._robot.open_jaw()
+                #else:
+            if self.mouse_buttons()[1] == 1 and self.previous_mouse_buttons[1] == 1:
+                    self._robot.close_jaw()
            
             self.previous_mouse_buttons[:] = self.mouse_buttons()
             time.sleep(0.03) # 0.03 is 30 ms, which is the spacenav's highest output frequency
