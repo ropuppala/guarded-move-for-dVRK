4f9f29d86bc4d4e1851c29b38293189f74cca984
Jennifer Hu <yhu32@jhu.edu>
2016-03-08 13:11:01 -0500

    fix some logic inconsistency

--- guarded_move.py
+++ guarded_move.py
@@ -1,4 +1,5 @@
-from dvrk.arm import *
+from dvrk.psm import *
+from joy_force import *
 
 class guarded_move(arm):
     """Simple robot API wrapping around ROS messages
@@ -7,16 +8,15 @@ class guarded_move(arm):
     def __init__(self, guarded_name, ros_namespace = '/dvrk/'):
         # first call base class constructor
         self._arm__init_arm(guarded_name, ros_namespace)
-        # publishers
-        self.set_jaw_position_publisher = rospy.Publisher(self._arm__full_ros_namespace
-                                                          + '/set_jaw_position',
-                                                          Float32, latch=True, queue_size = 1)
 
     def guarded_move_cartesian_translation(self, guarded_translation):
-        while (joy_force.zero_force()):
+        a = joy_force()
+        i = 0
+        while (a.zero_force() and i < 100):
             #new_x = guarded_translation[0]/100
             #new_y = guarded_translation[1]/100
             #new_z = guarded_translation[2]/100
             #self.delta_move_cartesian(new_x, new_y, new_z)
             print "here"
+            i = i +1
         print "done"#self.get_current_joint_position()
