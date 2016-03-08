9ceeb189b3f7e7f13d9a5e172aa7a0c707e06d8e
Jennifer Hu <yhu32@jhu.edu>
2016-03-08 14:25:55 -0500

    joy_force working - needs clean up

--- joy_force.py
+++ joy_force.py
@@ -3,16 +3,16 @@ import rospy
 from geometry_msgs.msg import Wrench
 
 class joy_force():
-    def callback(self, data):
-        self._f_x = data.force.x
-        self._f_y = data.force.y
-
     def __init__(self, ros_namespace = '/joystick1/wrench'):
         self._f_x = 0.0
         self._f_y = 0.0
         global previous 
         self.previous = 0
-        rospy.Subscriber('/joystick1/wrench', Wrench, self.callback)
+        rospy.Subscriber('/joystick1/wrench', Wrench, self.wrench_callback)
+
+    def wrench_callback(self, data):
+        self._f_x = data.force.x
+        self._f_y = data.force.y
     
     def zero_force (self):
         print 'x', self._f_x
@@ -22,7 +22,9 @@ class joy_force():
             if(not self._f_x == 0.0 or not self._f_y == 0.0):
                 global previous
                 self.previous = 1
-            return True
+                return False
+            else:
+                return True
         else:
             self.previous = 1
             print 'out'
