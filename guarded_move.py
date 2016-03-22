10384244f174b2c688a825af2042682b0a29e770
Jennifer Hu <yhu32@jhu.edu>
2016-03-22 16:17:50 -0400

    made guarded_move more usable

--- guarded_move.py
+++ guarded_move.py
@@ -1,21 +1,16 @@
 from dvrk.psm import *
-from joy_force import *
 import time
 
+#for the sensor there should be a function in that class called zero_force()
+#zero_force() must return False if force is not zero and True if force is zero
 class guarded_move(arm):
-    """Simple robot API wrapping around ROS messages
-    """
     # initialize the robot
     def __init__(self, guarded_name, ros_namespace = '/dvrk/'):
         # first call base class constructor
         self._arm__init_arm(guarded_name, ros_namespace)
 
-    def guarded_move_cartesian_translation(self, guarded_translation):
-        force = joy_force()
-        self._delta_guarded_move_cartesian_translation(guarded_translation, force)
-
-    def _delta_guarded_move_cartesian_translation(self, guarded_translation, force):
-        i = 0
+    def delta_guarded_move_cartesian_translation(self, guarded_translation, force_class):
+        #intitalizing and declaring variables needed
         new_x = 0
         new_y = 0
         new_z = 0
@@ -23,12 +18,12 @@ class guarded_move(arm):
         delta_y = guarded_translation[1]/1000
         delta_z = guarded_translation[2]/1000
 
-        while (force.zero_force() and i < 100):
+        #keep going until force is no longer zero, if force is no longer
+        #zero
+        while (force_class.zero_force()):
             new_x = delta_x + new_x
             new_y = delta_y + new_y
             new_z = delta_z + new_z
 
             self.delta_move_cartesian_translation([new_x, new_y, new_z])
-            i = i + 1
-            #time.sleep (0.01)
-        print "Found :", self.get_current_joint_position()
+        print "Found at Position : ", self.get_current_cartesian_position().p
