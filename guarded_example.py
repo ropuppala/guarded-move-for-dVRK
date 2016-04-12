c93e3d3002489adf083a99c14c61a44036a4cea2
Jennifer Hu <yhu32@jhu.edu>
2016-04-12 15:54:57 -0400

    added guarded move for PyKDL Frame

--- guarded_example.py
+++ guarded_example.py
@@ -11,7 +11,21 @@ else:
     robotName = sys.argv[1]
     r = guarded_move(robotName)
     r.home()
-    #r.move_cartesian_translation([0.0,0.0,-0.1])
+    r.move_cartesian_translation([0.0,0.0,-0.1])
     print "Start at Position : ", r.get_desired_cartesian_position().p
-    time.sleep(2)
-    r.delta_guarded_move_cartesian_translation([0.1,0.0,0.0], joy_condition().zero_force)
+    time.sleep(1)
+    
+    A = Vector(0.01,0.05,0.0)
+    B = Rotation.Identity()
+    move = Frame(B,A)
+    if (r.guarded_move_cartesian_frame(move, joy_condition().zero_force)):
+        print "Found : ", r.get_desired_cartesian_position().p
+        time.sleep(1)
+        next = Vector(0.01,0.0,0.0)
+        r.guarded_move_cartesian_translation(next, joy_condition().zero_force)
+    else:
+        print "Not Found : ", r.get_desired_cartesian_position().p
+        time.sleep(1)
+        third = [0.0,-0.01,0.0]
+        r.guarded_move_cartesian_translation(third, joy_condition().zero_force)
+        print "Done : ", r.get_desired_cartesian_position().p
--- guarded_move.py
+++ guarded_move.py
@@ -7,35 +7,41 @@ class guarded_move(arm):
         # first call base class constructor
         self._arm__init_arm(guarded_name, ros_namespace)
 
+    #Guarded Move using Vector/List
     def guarded_move_cartesian_translation(self, guarded_translation, condition_method):
-        if(self.__check_input_type(guarded_translation, [list, Vector])):
-            if(type(guarded_translation) is list):
-                if (self.__check_list_length(guarded_translation, 3)):
-                    # convert into a Vector
-                    guarded_vector = Vector(guarded_translation[0], guarded_translation[1], guarded_translation[2])
-                else:
-                    return
+        if(type(guarded_translation) is list):
+            if (len(guarded_translation) == 3):
+                # convert into a Vector
+                guarded_vector = Vector(guarded_translation[0], guarded_translation[1], guarded_translation[2])
             else:
-                guarded_vector = guarded_translation
-            # convert into a Frame
-            guarded_rotation = Rotation.Identity()
-            guarded_frame = Frame(guarded_rotation, guarded_vector)
-            # move accordingly
-            self.guarded_move_cartesian_frame(guarded_frame, condition_method)
+                print "needs be in Cartesian Coordinate System [x,y,z]"
+                return
+        elif (type(guarded_translation) is Vector):
+            guarded_vector = guarded_translation
+        else:
+            print "Error: Not a list or a vector"
+        # convert into a Frame
+        guarded_rotation = Rotation.Identity()
+        guarded_frame = Frame(guarded_rotation, guarded_vector)
+        print guarded_frame
+        # move accordingly
+        self.guarded_move_cartesian_frame(guarded_frame, condition_method)
 
-    def guarded_move_carteisian_frame (self, guarded_frame, condition_method):
+    #Guarded Move using PyKDL Frame on default
+    #velocity = 0.00001 and time_step = 0.01
+    def guarded_move_cartesian_frame (self, guarded_frame, condition_method):
         #intitalizing and declaring variables needed
         velocity = 0.00001 #m/s
         time_step = 0.01  #rate in Hertz
         displacement = math.sqrt(dot(guarded_frame.p, guarded_frame.p))
-            #math.pow(guarded_translation[0],2) + math.pow(guarded_translation[1],2)
-             #                    +math.pow(guarded_translation[2],2))
-        total_time = displacement / velocity
-        nb_points = total_time * time_step
-        delta_translation = guarded_translation / nb_points
+        #total_time = displacement / velocity
+        nb_points = (displacement / velocity) * time_step
+        #delta_translation = guarded_frame.p * (1/nb_points)
+        #delta_rotation = Rotation.Identity()
+        delta_frame = Frame (Rotation.Identity(), guarded_frame.p * (1 / nb_points))
         i = 0
         while (condition_method() and i < nb_points):
-            self.delta_move_cartesian_translation(delta_translation, False)
+            self.delta_move_cartesian_frame(delta_frame, True)
             i = i+1
         if (i < nb_points):
             return True
